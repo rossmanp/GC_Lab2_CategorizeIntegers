00bb29d972bfb15d88c037b23224740deacf81f9
Pete Rossman <pete.rossman@gmail.com>
2017-03-28 13:05:49 -0400

    First commit.

---
+++ GC_Lab2_CategorizeIntegers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab2_CategorizeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
