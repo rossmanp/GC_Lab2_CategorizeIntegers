c2e853c07c5ec67a676c3c5bf0f3a69298891cac
Pete Rossman <pete.rossman@gmail.com>
2017-03-28 13:09:19 -0400

    add do while loop to loop input command until proper input is obtained

--- GC_Lab2_CategorizeIntegers/Program.cs
+++ GC_Lab2_CategorizeIntegers/Program.cs
@@ -10,6 +10,35 @@ namespace GC_Lab2_CategorizeIntegers
     {
         static void Main(string[] args)
         {
+            int myInt = 0;
+            do
+            {
+                Console.WriteLine("Type in an integer from among 1 to 100: ");
+                myInt = int.Parse(Console.ReadLine());
+                if (myInt < 1 || myInt > 100)
+                {
+                    Console.WriteLine("The integer you input is not an integer from 1 to 100!");
+                }
+            }
+            while (myInt < 1 || myInt > 100);
+
+                if (myInt % 2 == 1)
+            {
+                Console.WriteLine("Your number is " + myInt + " and it is odd.");
+            }
+            else if (myInt > 60)
+            {
+                Console.WriteLine("Your number is " + myInt + " and it is even.");
+            }
+            else if (myInt <= 60 && myInt >= 26)
+            {
+                Console.WriteLine("Your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is even and less than 25.");
+            }
+            Console.ReadLine();
         }
     }
 }
