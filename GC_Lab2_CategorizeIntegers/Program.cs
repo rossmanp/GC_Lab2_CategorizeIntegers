96e938e17454e01b8a0675c04fe97a502c0f92e5
Pete Rossman <pete.rossman@gmail.com>
2017-03-28 13:39:59 -0400

    Added more comments and proofread output

--- GC_Lab2_CategorizeIntegers/Program.cs
+++ GC_Lab2_CategorizeIntegers/Program.cs
@@ -16,15 +16,16 @@ namespace GC_Lab2_CategorizeIntegers
         static void Main(string[] args)
         {
             int myInt = 0;
-            string name;
-            string runAgain;
-            Console.Write("Please enter your name: ");
-            name = Console.ReadLine();
+            string runAgain;           
             bool runProgram = true;
+
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
             while (runProgram)
                 { 
-            //The do while loop runs until an integer from 1 to 100
-            //is obtained as input.
+                     //The do while loop runs until an integer from 1 to 100
+                     //is obtained as input.
                      do
                      {
                        Console.WriteLine(name + ", type in an integer from among 1 to 100: ");
@@ -36,33 +37,38 @@ namespace GC_Lab2_CategorizeIntegers
                       }
                       while (myInt < 1 || myInt > 100);
 
-                //The following conditionals display whether the input is odd or even
-                //and in some cases print the number.
+                    //The following conditionals display whether the input is odd or even
+                    //and in some cases print the number.
 
-                if (myInt % 2 == 1)
-                {
-                    Console.WriteLine(name + ", your number is " + myInt + " and it is odd.");
-                }
-                else if (myInt > 60)
-                {
-                    Console.WriteLine(name + ", Your number is " + myInt + " and it is even.");
-                }
-                else if (myInt <= 60 && myInt >= 26)
-                {
-                    Console.WriteLine(name + ", Your number is even.");
-                }
-                else
-                {
-                    Console.WriteLine(name + ", Your number is even and less than 25.");
-                }
-                Console.Write("Do you want to input another integer " + name + "? (y/n):");
-                runAgain = Console.ReadLine();
-                if (runAgain.ToLower() == "n")
-                {
-                    Console.WriteLine("Goodbye" + name + " !");
-                    runProgram = false;
-                    Console.ReadLine();
-                }
+                    if (myInt % 2 == 1)
+                    {
+                        Console.WriteLine(name + ", your number is " + myInt + " and it is odd.");
+                    }
+                    else if (myInt > 60)
+                    {
+                        Console.WriteLine(name + ", your number is " + myInt + " and it is even.");
+                    }
+                    else if (myInt <= 60 && myInt >= 26)
+                    {
+                        Console.WriteLine(name + ", your number is even.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(name + ", your number is even and less than 25.");
+                    }
+
+                    //The user is asked if he or she wants to use the program again; if no
+                    //is indicated, the program exits.
+
+                    Console.Write("Do you want to input another integer, " + name + "? (y/n):");
+                    runAgain = Console.ReadLine();
+
+                    if (runAgain.ToLower() == "n")
+                    {
+                        Console.WriteLine("Goodbye" + name + " !");
+                        runProgram = false;
+                        Console.ReadLine();
+                    }
         }
       }
    }
