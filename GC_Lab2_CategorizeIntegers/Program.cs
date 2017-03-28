910207b8a5298943dacf51d5714654a7a8e0fa8d
Pete Rossman <pete.rossman@gmail.com>
2017-03-28 13:20:05 -0400

    add user name inputand output

--- GC_Lab2_CategorizeIntegers/Program.cs
+++ GC_Lab2_CategorizeIntegers/Program.cs
@@ -16,15 +16,19 @@ namespace GC_Lab2_CategorizeIntegers
         static void Main(string[] args)
         {
             int myInt = 0;
+            string name;
             //The do while loop runs until an integer from 1 to 100
             //is obtained as input.
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
             do
             {
-                Console.WriteLine("Type in an integer from among 1 to 100: ");
+                Console.WriteLine(name + ", type in an integer from among 1 to 100: ");
                 myInt = int.Parse(Console.ReadLine());
                 if (myInt < 1 || myInt > 100)
                 {
-                    Console.WriteLine("The integer you input is not an integer from 1 to 100!");
+                    Console.WriteLine("The integer you input is not an integer from 1 to 100 " + name + " !");
                 }
             }
             while (myInt < 1 || myInt > 100);
@@ -34,19 +38,19 @@ namespace GC_Lab2_CategorizeIntegers
 
             if (myInt % 2 == 1)
             {
-                Console.WriteLine("Your number is " + myInt + " and it is odd.");
+                Console.WriteLine(name + ", your number is " + myInt + " and it is odd.");
             }
             else if (myInt > 60)
             {
-                Console.WriteLine("Your number is " + myInt + " and it is even.");
+                Console.WriteLine(name + ", Your number is " + myInt + " and it is even.");
             }
             else if (myInt <= 60 && myInt >= 26)
             {
-                Console.WriteLine("Your number is even.");
+                Console.WriteLine(name + ", Your number is even.");
             }
             else
             {
-                Console.WriteLine("Your number is even and less than 25.");
+                Console.WriteLine(name + ", Your number is even and less than 25.");
             }
             Console.ReadLine();
         }
